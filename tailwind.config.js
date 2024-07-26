bceb6b2a48fe49cb77f1eb421138392ad50c475f
Daniel Aldridge <daniel.aldridge@gmail.com>
2024-07-26 17:01:58 -0600

    Added Flowbite and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // add this line
+     "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin') // add this line
+  ],
+}
+
+
+
+
